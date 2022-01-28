d148903c5e319516119314d8460e0d7348c54824
Manivannan Nagarajan <cibimn@gmail.com>
2022-01-29 00:51:49 +0530

    Created Super User and Leads-Agents

---
+++ leads/models.py
@@ -0,0 +1,35 @@
+from tkinter import CASCADE
+from tokenize import blank_re
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+ 
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    
+    # Source_Choices = {
+    #     ('YT','Youtube'),
+    #     ('Google','Google'),
+    #     ('Newsletter','Newsletter')
+    # }
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    # Phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=Source_Choices, max_length=100)
+
+    # profile_picure = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+    
